9bdeca60d6a8b6e06206bdedaf80c363f9347199
Mari Roberts <32748142+mr4909@users.noreply.github.com>
2020-01-22 10:18:26 -0500

    update analysis

--- tiger/analysis-r/analysis.R
+++ tiger/analysis-r/analysis.R
@@ -6,11 +6,16 @@
 # import data and load libraries
 source('import.R')
 
+source('functions_modified.R')
+
 # clean presence background data
 source('pb.cleaning.R')
 
 # clean presence absence data
 source('pa.cleaning.R')
+
+# clean camera trap absence data
+source('ct.cleaning.R')
 
 ########################################
 # Analysis
@@ -26,7 +31,7 @@ pa.fit=so.model(X.so,y.so)
 # Presence background model
 ########################
 
-pb.fit=pb.ipp(X.po, W.po, X.back, W.back)
+pb.fit=pb.ipp(X.pb, X.back)
 
 ########################
 # Integrated model
