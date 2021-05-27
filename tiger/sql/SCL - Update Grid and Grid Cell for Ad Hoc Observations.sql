ee396e6ae5ca8caeec7069895a3a3ac975231b55
Steven Gallo <sgallo@wcs.org>
2021-05-27 14:10:46 -0400

    SQL Scripts
    Adding SQL scripts to update Grid and GridCell values in CT Deployment and Ad Hoc Observation

---
+++ tiger/sql/SCL - Update Grid and Grid Cell for Ad Hoc Observations.sql
@@ -0,0 +1,26 @@
+SELECT
+	CI_AdHocObservation.CI_GridID
+	,CI_AdHocObservation.CI_GridCellID
+	,CI_GridCell.CI_GridCellCode
+	,G2.CI_GridCellCode
+	,G2.CI_GridID
+	,G2.CI_GridCellID
+FROM
+	CI_AdHocObservation
+	INNER JOIN CI_GridCell ON (CI_AdHocObservation.CI_GridCellID=CI_GridCell.CI_GridCellID)
+	INNER JOIN CI_GridCell G2 ON (CI_GridCell.CI_GridCellCode=G2.CI_GridCellCode AND G2.CI_GridID=19)
+WHERE
+	CI_AdHocObservation.CI_GridID=18
+
+/*
+--25,090 rows
+UPDATE CI_AdHocObservation
+SET CI_GridID=G2.CI_GridID
+	,CI_GridCellID=G2.CI_GridCellID
+FROM
+	CI_AdHocObservation
+	INNER JOIN CI_GridCell ON (CI_AdHocObservation.CI_GridCellID=CI_GridCell.CI_GridCellID)
+	INNER JOIN CI_GridCell G2 ON (CI_GridCell.CI_GridCellCode=G2.CI_GridCellCode AND G2.CI_GridID=19)
+WHERE
+	CI_AdHocObservation.CI_GridID=18
+*/
---
+++ tiger/sql/SCL - Update Grid and Grid Cell for CT Deployment.sql
@@ -0,0 +1,25 @@
+SELECT
+	CI_CameraTrapDeployment.CI_GridID
+	,CI_CameraTrapDeployment.CI_GridCellID
+	,CI_GridCell.CI_GridCellCode
+	,G2.CI_GridCellCode
+	,G2.CI_GridID
+	,G2.CI_GridCellID
+FROM
+	CI_CameraTrapDeployment
+	INNER JOIN CI_GridCell ON (CI_CameraTrapDeployment.CI_GridCellID=CI_GridCell.CI_GridCellID)
+	INNER JOIN CI_GridCell G2 ON (CI_GridCell.CI_GridCellCode=G2.CI_GridCellCode AND G2.CI_GridID=19)
+WHERE
+	CI_CameraTrapDeployment.CI_GridID=18
+
+/*--972 rows
+UPDATE CI_CameraTrapDeployment
+SET CI_GridID=G2.CI_GridID
+	,CI_GridCellID=G2.CI_GridCellID
+FROM
+	CI_CameraTrapDeployment
+	INNER JOIN CI_GridCell ON (CI_CameraTrapDeployment.CI_GridCellID=CI_GridCell.CI_GridCellID)
+	INNER JOIN CI_GridCell G2 ON (CI_GridCell.CI_GridCellCode=G2.CI_GridCellCode AND G2.CI_GridID=19)
+WHERE
+	CI_CameraTrapDeployment.CI_GridID=18
+*/
